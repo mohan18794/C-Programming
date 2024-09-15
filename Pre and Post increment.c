15acf6154eadefa5283409607a433210698b28d5
MOHAN SINGIREDDI <125856165+mohan18794@users.noreply.github.com>
2024-09-15 17:28:58 +0530

    Update Pre and Post increment.c

--- Pre and Post increment.c
+++ Pre and Post increment.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    int a=7;
+    int b=a++ * ++a* a--;
+    int c=7;
+    int d=a++;
+    printf("%d\n %d\n %d\n %d\n",a,b,c,d);
+    printf("Hello World");
+
+    
+}
 
