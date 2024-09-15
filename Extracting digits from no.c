b90c8ff98a69402b5dedb0db11679879ba3918c3
MOHAN SINGIREDDI <125856165+mohan18794@users.noreply.github.com>
2024-09-15 17:35:09 +0530

    Update Extracting digits from no.c

--- Extracting digits from no.c
+++ Extracting digits from no.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main()
+{
+    int n=2576;
+    int r;
+    int x=0;
+    while(n>0)
+    {
+        r=n%10;
+        n=n/10;
+        printf("%d",r);
+        //printf("\n");
+
+    } 
+    
+    
+   
+    return 0;
+}
