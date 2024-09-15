cdba81c2ffaff0dacbd687f2a12efe9a95b43e80
MOHAN SINGIREDDI <125856165+mohan18794@users.noreply.github.com>
2024-09-15 17:32:07 +0530

    Update goto.c

--- goto.c
+++ goto.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=3,b=4,flag=0;
+    if(a<b){
+        label1:
+        printf("a\n");
+        if(flag==0)
+        {
+            flag=1;
+            goto label2;
+        }
+    label2:
+    printf("b\n");
+    if(flag==0)
+    {
+       flag=1;
+        goto label1;
+    
+    }
+    return 0;
+
+}
+}
 
