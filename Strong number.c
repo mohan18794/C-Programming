51736345e6b5efc00db660bd1396a420da6ee0d0
MOHAN SINGIREDDI <125856165+mohan18794@users.noreply.github.com>
2024-09-15 17:38:49 +0530

    Update Strong number.c

--- Strong number.c
+++ Strong number.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int n=145;
+    int r=1,sum=0,i,x;
+    int temp=n;
+    while(n>0)
+    {
+        x=n%10;
+        n=n/10;
+    
+    for(i=x;i>1;i--)
+    {
+        r=r*i;
+    }
+    sum=sum+r;
+    r=1;
+    }
+    if(sum==temp)
+    {
+        printf("The no is strong number");
+    }
+    else
+    {
+        printf("The no is not strong number");
+    }
+    return 0;
+}
 
