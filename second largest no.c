b71f69866805f1fd64e12ef6a4dcb3c17ab71008
MOHAN SINGIREDDI <125856165+mohan18794@users.noreply.github.com>
2024-09-15 17:33:40 +0530

    Update second largest no.c

--- second largest no.c
+++ second largest no.c
@@ -1,1 +1,48 @@
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d;
+    printf("enter a value\n");
+    scanf("%d",&a);
+    printf("enter b value\n");
+    scanf("%d",&b);
+    printf("enter c value\n");
+    scanf("%d",&c);
+    //printf("enter d value\n");
+    //scanf("%d",&c);
+   if(a>b&&a>c){
+       if(b>c){
+           d=b;
+       }
+       else{
+           d=c;
+       }
+   
+   }
+    else if(b>c&&b>a){
+        if(a>c){
+            d=a;
+        }
+        else{
+            d=c;
+        }
+    }
+     else{
+         if(b>a){
+             d=b;
+         }
+         else{
+             d=a;
+         }
+         
+     
+    }
+    printf("%d",d);
+      
+        
+
+    return 0;
+}
 
